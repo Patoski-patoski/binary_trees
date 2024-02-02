2020cdd883b90dba3e18a6ea34edb85d288155e1
patrick okafor <patrickpeters911@gmail.com>
2024-02-02 05:32:52 -0800

    a function that inserts a node as the left-child of another node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,7 +23,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (newNode == NULL)
 		return (NULL);
 
-	binary_tree_t *oldLeftChild = parent->left;
+	binary_tree_t *oldLeftChild;
+
+	oldLeftChild = parent->left;
 
 	if (oldLeftChild != NULL)
 	{
