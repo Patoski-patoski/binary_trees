1c970c7f0a9ab6076be8263e703041ac43546e8a
patrick okafor <patrickpeters911@gmail.com>
2024-02-03 17:50:27 -0800

    a function that checks if a node is a leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf- a function that checks if a node is a leaf
+ * @node:
+ *
+ * Return: On Success, 1, otherwise 0
+ **/
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->left || node->right)
+		return (0);
+	return (1);
+}
