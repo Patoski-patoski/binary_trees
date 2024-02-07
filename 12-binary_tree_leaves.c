bf2d7c6180ea40c4d6685f989c60f4b435b24a14
patrick okafor <patrickpeters911@gmail.com>
2024-02-07 04:06:13 -0800

    a function that counts the leaves in a binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,25 +10,14 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leftLeaf = 0;
-	size_t rightLeaf = 0;
+	size_t result = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-	{
-		if (tree->left)
-			leftLeaf = binary_tree_leaves(tree->left->left);
-		if (tree->right)
-			leftLeaf = binary_tree_leaves(tree->left->right);
-	}
-	if (tree->right)
-	{
-		if (tree->right)
-			rightLeaf = binary_tree_leaves(tree->right->right);
-		if (tree->left)
-			rightLeaf = binary_tree_leaves(tree->right->left);
-	}
-	return ((rightLeaf + leftLeaf) + 1);
+	if (tree->right == NULL && tree->left == NULL)
+		return (1);
+       
+	result = binary_tree_leaves(tree->left) +  binary_tree_leaves(tree->right);
+	return (result);
 }
