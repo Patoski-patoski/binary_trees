339812ed0013058cc3594c9efb3b070024cb9652
patrick okafor <patrickpeters911@gmail.com>
2024-02-08 06:31:13 -0800

    a function that checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -4,15 +4,15 @@
  * binary_tree_is_perfect- a function that checks if a binary tree is perfect
  * @tree: a pointer to the root node of the tree to check
  *
- * Return: 1 if tree isperfect, othewise 0
+ * Return: 1 if tree isperfect, otherwise 0
  *
  **/
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int rightPerfect = 0;
-	int leftPerfect = 0;
-	
+	int rightPerfect;
+	int leftPerfect;
+
 	if (tree == NULL)
 		return (0);
 
